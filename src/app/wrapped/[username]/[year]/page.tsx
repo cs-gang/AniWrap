2a4c95b75d85fe4b33204e63b398661552a1daa4
shriram-sg <shriramsekar11@gmail.com>
2025-07-06 08:53:07 +0400

    feat: make a vertical carousel and the first section

--- src/app/wrapped/[username]/[year]/page.tsx
+++ src/app/wrapped/[username]/[year]/page.tsx
@@ -1,13 +1,69 @@
-export default function Wrapped() {
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export default async function Wrapped({ params }: { params: Promise<{ username: string, year: string }> }) {
+    const { username, year } = await params
+
+    const mockData = {
+        avatar: "https://github.com/shadcn.png",
+        overallMsg: "This year, you watched a variety of different anime!"
+    }
 
     return (
-        <div className="flex flex-col gap-4 h-screen w-screen justify-center items-center">
-            <div className="text-7xl">
-                Aniwrap
-            </div>
-            <div className="text-xl italic laquer-regular">
-                WRAPPED
-            </div>
-        </div>
+        <Carousel orientation="vertical">
+            <CarouselContent>
+                <CarouselItem>
+                    <div className="flex flex-col gap-4 h-screen w-screen justify-center items-center">
+                        <div>
+                            <div className="flex gap-4 items-center">
+                                <Avatar className="w-28 h-28">
+                                    <AvatarImage src={mockData.avatar} />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <div className="text-7xl text-blue-700">
+                                    {username}'s
+                                </div>
+                            </div>
+                            <div className="text-8xl">
+                                ANIWRAP {year}
+                            </div>
+                            <div className="mt-16 flex flex-col gap-12">
+                                <div className="text-4xl">{mockData.overallMsg}</div>
+                                <div className="text-3xl">Here are a few of your highlights...</div>
+                            </div>
+                        </div>
+                    </div>
+                </CarouselItem>
+                <CarouselItem>
+                    <div className="flex flex-col gap-4 h-screen w-screen justify-center items-center bg-blue-200">
+                        <div>
+                            <div className="flex gap-4 items-center">
+                                <Avatar className="w-28 h-28">
+                                    <AvatarImage src={mockData.avatar} />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <div className="text-7xl text-blue-700">
+                                    {username}'s
+                                </div>
+                            </div>
+                            <div className="text-8xl">
+                                ANIWRAP {year}
+                            </div>
+                            <div className="mt-16 flex flex-col gap-12">
+                                <div className="text-4xl">{mockData.overallMsg}</div>
+                                <div className="text-3xl">Here are a few of your highlights...</div>
+                            </div>
+                        </div>
+                    </div>
+                </CarouselItem>
+            </CarouselContent>
+        </Carousel>
     );
 }
